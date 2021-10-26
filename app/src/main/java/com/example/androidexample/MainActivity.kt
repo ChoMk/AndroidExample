f58671857e333b18130536f75421b5ea0cf5a824
myeongki.cho <myeongki.cho@navercorp.com>
2021-10-26 20:47:37 +0900

    버튼 클릭 예제

--- app/src/main/java/com/example/androidexample/MainActivity.kt
+++ app/src/main/java/com/example/androidexample/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.androidexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        findViewById<TextView>(R.id.sample_text_view).setOnClickListener {
+            Toast.makeText(this, "text click", Toast.LENGTH_SHORT).show()
+        }
+        findViewById<TextView>(R.id.sample_button_view).setOnClickListener {
+            Toast.makeText(this, "button click", Toast.LENGTH_SHORT).show()
+        }
     }
 }
